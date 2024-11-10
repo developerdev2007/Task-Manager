e2f6252a75da68777189e11bf0ac718ccea1987f
developerdev2007 <160243215+developerdev2007@users.noreply.github.com>
2024-11-10 12:00:27 +0530

    build tsc

--- backend/dist/routes/taskRoute.js
+++ backend/dist/routes/taskRoute.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const taskController_1 = require("../controllers/taskController");
 const taskRoutes = express_1.default.Router();
-taskRoutes.get("/task", taskController_1.getTasks); //all tasks
+taskRoutes.get("/tasks", taskController_1.getTasks); //all tasks
 taskRoutes.get("/task/:id", taskController_1.getTaskById); // by id
 taskRoutes.post("/task", taskController_1.createTask); // create task
 taskRoutes.delete("/task/:id", taskController_1.deleteTask); //delete task
